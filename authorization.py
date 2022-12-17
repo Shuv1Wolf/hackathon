6146a901d52ae19c3ce055cf9812d9161974919f
Shuv1Wolf <116355740+Shuv1Wolf@users.noreply.github.com>
2022-12-17 23:13:06 +0300

    добавлено хеширование логина и пароля

--- authorization.py
+++ authorization.py
@@ -1,4 +1,5 @@
 import sqlite3
+import hashlib
 
 class Authorization:
 
@@ -18,9 +19,11 @@ class Authorization:
         self.cursor.execute(sqlite_select_query)
         result = self.cursor.fetchall()
         flag = False
+        hach_log = hashlib.sha384(login.encode())
+        hach_pas = hashlib.sha384(password.encode())
         for i in range(len(result)):
             users = result[i]
-            if users[0] == login and users[1] == password:
+            if users[0] == hach_log.hexdigest() and users[1] == hach_pas.hexdigest():
                 flag = True
         return flag
 
@@ -29,9 +32,13 @@ class Authorization:
         table -> сама редактируемая таблица в БД
         user_log -> логин пользователя, который мы регистрируем
         password -> пароль пользователя, который мы регистрируем"""
-        request = f"""INSERT INTO {table} VALUES('{user_log}', '{password}')"""
+        hash_log = hashlib.sha384(user_log.encode())
+        hash_pas = hashlib.sha384(password.encode())
+        request = f"""INSERT INTO {table} VALUES('{hash_log.hexdigest()}', '{hash_pas.hexdigest()}')"""
         self.cursor.execute(request)
         return self.connection.commit()
+
+
 
 
 
