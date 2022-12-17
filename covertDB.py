9662a4164fd5819ab3b789c3fe339e8bdcd20296
Shuv1Wolf <116355740+shuv1wolf@users.noreply.github.com>
2022-12-18 00:42:38 +0300

    +func

--- covertDB.py
+++ covertDB.py
@@ -12,9 +12,14 @@ class Convert:
         """функция для конвертирования БД в таблицу excel"""
         name = getpass.getuser()
         df1 = pd.read_sql('select * from product', self.connection)
-        df1.to_excel(rf'C:\Users\{name}\Downloads' + r'\file.xlsx', index=False)
-        df2 = pd.read_excel(rf'C:\Users\{name}\Downloads' + r'\file.xlsx')
-        df2.to_csv(rf'C:\Users\{name}\Downloads' + r'\file.csv', index=False)
+        df1.to_excel(rf'C:\Users\{name}\Downloads' + r'\table_products.xlsx', index=False)
+        df2 = pd.read_excel(rf'C:\Users\{name}\Downloads' + r'\table_products.xlsx')
+        df2.to_csv(rf'C:\Users\{name}\Downloads' + r'\table_products.csv', index=False)
+
+        df3 = pd.read_sql('select * from order1', self.connection)
+        df3.to_excel(rf'C:\Users\{name}\Downloads' + r'\table_orders.xlsx', index=False)
+        df4 = pd.read_excel(rf'C:\Users\{name}\Downloads' + r'\table_orders.xlsx')
+        df4.to_csv(rf'C:\Users\{name}\Downloads' + r'\table_orders.csv', index=False)
 
 
 
